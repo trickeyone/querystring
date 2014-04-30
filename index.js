0ae28aabe0ab47931b9a7a51ab195d23ab87d718
Josh England <joshua.en@gmail.com>
2014-04-29 17:29:39 -0700

    - moving decode/encode into index.js for AMD/require - updating version 0.2.1

--- index.js
+++ index.js
@@ -1,4 +1,166 @@
 'use strict';
 
-exports.decode = exports.parse = require('./decode');
-exports.encode = exports.stringify = require('./encode');
+;(function() {
+
+  var decode = (function(){
+    // If obj.hasOwnProperty has been overridden, then calling
+// obj.hasOwnProperty(prop) will break.
+// See: https://github.com/joyent/node/issues/1707
+    function hasOwnProperty(obj, prop) {
+      return Object.prototype.hasOwnProperty.call(obj, prop);
+    }
+
+    return function(qs, sep, eq, options) {
+      sep = sep || '&';
+      eq = eq || '=';
+      var obj = {};
+
+      if (typeof qs !== 'string' || qs.length === 0) {
+        return obj;
+      }
+
+      var regexp = /\+/g;
+      qs = qs.split(sep);
+
+      var maxKeys = 1000;
+      if (options && typeof options.maxKeys === 'number') {
+        maxKeys = options.maxKeys;
+      }
+
+      var len = qs.length;
+      // maxKeys <= 0 means that we should not limit keys count
+      if (maxKeys > 0 && len > maxKeys) {
+        len = maxKeys;
+      }
+
+      for (var i = 0; i < len; ++i) {
+        var x = qs[i].replace(regexp, '%20'),
+          idx = x.indexOf(eq),
+          kstr, vstr, k, v;
+
+        if (idx >= 0) {
+          kstr = x.substr(0, idx);
+          vstr = x.substr(idx + 1);
+        } else {
+          kstr = x;
+          vstr = '';
+        }
+
+        k = decodeURIComponent(kstr);
+        v = decodeURIComponent(vstr);
+
+        if (!hasOwnProperty(obj, k)) {
+          obj[k] = v;
+        } else if (Array.isArray(obj[k])) {
+          obj[k].push(v);
+        } else {
+          obj[k] = [obj[k], v];
+        }
+      }
+
+      return obj;
+    };
+  })();
+
+  var encode = (function(){
+    var stringifyPrimitive = function(v) {
+      switch (typeof v) {
+      case 'string':
+        return v;
+
+      case 'boolean':
+        return v ? 'true' : 'false';
+
+      case 'number':
+        return isFinite(v) ? v : '';
+
+      default:
+        return '';
+      }
+    };
+
+    return function(obj, sep, eq, name) {
+      sep = sep || '&';
+      eq = eq || '=';
+      if (obj === null) {
+        obj = undefined;
+      }
+
+      if (typeof obj === 'object') {
+        return Object.keys(obj).map(function(k) {
+          var ks = encodeURIComponent(stringifyPrimitive(k)) + eq;
+          if (Array.isArray(obj[k])) {
+            return obj[k].map(function(v) {
+              return ks + encodeURIComponent(stringifyPrimitive(v));
+            }).join(sep);
+          } else {
+            return ks + encodeURIComponent(stringifyPrimitive(obj[k]));
+          }
+        }).join(sep);
+
+      }
+
+      if (!name) return '';
+      return encodeURIComponent(stringifyPrimitive(name)) + eq +
+             encodeURIComponent(stringifyPrimitive(obj));
+    };
+  })();
+
+  var querystring = {
+    decode: decode,
+    encode: encode,
+    parse: decode,
+    stringify: encode
+  };
+
+  /** Used to determine if values are of the language type Object */
+  var objectTypes = {
+    'boolean': false,
+    'function': true,
+    'object': true,
+    'number': false,
+    'string': false,
+    'undefined': false
+  };
+
+  /** Used as a reference to the global object */
+  var root = (objectTypes[typeof window] && window) || this;
+
+  /** Detect free variable `exports` */
+  var freeExports = objectTypes[typeof exports] && exports && !exports.nodeType && exports;
+
+  /** Detect free variable `module` */
+  var freeModule = objectTypes[typeof module] && module && !module.nodeType && module;
+
+  /** Detect the popular CommonJS extension `module.exports` */
+  var moduleExports = freeModule && freeModule.exports === freeExports && freeExports;
+
+  // some AMD build optimizers like r.js check for condition patterns like the following:
+  if (typeof define == 'function' && typeof define.amd == 'object' && define.amd) {
+    // Expose Lo-Dash to the global object even when an AMD loader is present in
+    // case Lo-Dash is loaded with a RequireJS shim config.
+    // See http://requirejs.org/docs/api.html#config-shim
+    root._ = _;
+
+    // define as an anonymous module so, through path mapping, it can be
+    // referenced as the "underscore" module
+    define(function() {
+      return querystring;
+    });
+  }
+  // check for `exports` after `define` in case a build optimizer adds an `exports` object
+  else if (freeExports && freeModule) {
+    // in Node.js or RingoJS
+    if (moduleExports) {
+      (freeModule.exports = querystring).querystring = querystring;
+    }
+    // in Narwhal or Rhino -require
+    else {
+      freeExports.querystring = querystring;
+    }
+  }
+  else {
+    // in a browser or Rhino
+    root.querystring = querystring;
+  }
+}).call(this);
